0583b79842831a391bc8e3ea67e3543df7a22cb3
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2018-09-12 13:34:02 +0700

    Add files via upload

--- silent.py
+++ silent.py
@@ -987,7 +987,7 @@ def bot(op):
                                       except:
                                           pass
               return
-         if op.type == 32:
+        if op.type == 32:
             if op.param3 in Dmid:
               if op.param2 not in Bots and op.param2 not in owner and op.param2 not in admin and op.param2 not in staff:
                   wait["blacklist"][op.param2] = True
